d4d77e04adad573cb0e12be251f622435f30ac86
Craig Astill <craigastill@gmail.com>
2018-08-15 00:54:07 +0100

    Add setup.py

---
+++ anagram_finder/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anagram_finder",
+    version="0.0.1",
+    author="Craig Astill",
+    author_email="craigastill@gmail.com",
+    description="An anagram finding CLI homework application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
