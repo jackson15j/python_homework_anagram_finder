d4538b153d4f0c5eb23438db7b624342ba9ff1ab
Craig Astill <craigastill@gmail.com>
2018-08-14 10:26:17 +0100

    Use pytest fixtures to do generic testcase setup.

--- test/test_anagram_finder.py
+++ test/test_anagram_finder.py
@@ -1,24 +1,34 @@
 from .. import anagram_finder
+import pytest
 
 
+@pytest.fixture()
+def anagramFinder():
+    """Generic setup of an `AnagramFinder` instance."""
+    return anagram_finder.AnagramFinder()
+
+
+@pytest.mark.usefixtures("anagramFinder")
 class TestAnagramFinder(object):
-    def test_filter_source(self):
+    """Unittests for the AnagramFinder class."""
+
+    def test_filter_source(self, anagramFinder):
+        """Positively verify `_filter_source()`."""
         a = "a fake source with punctuation. And new\nlines\ninside it. fake."
         exp = {
             "a", "fake", "source", "with", "punctuation", "and", "new",
             "lines", "inside", "it"}
 
-        anagramFinder = anagram_finder.AnagramFinder()
         result = anagramFinder._filter_source(a)
         assert exp == result
 
-    def test_filter_anagram_lists(self):
+    def test_filter_anagram_lists(self, anagramFinder):
+        """Positively verify `_filter_anagram_lists()`."""
         a = ['a', 'b', 'c']
         b = ['b', 'c', 'a']
         c = ['d', 'e']
         d = [a, a, b, b, c, c]
         exp = {tuple(a), tuple(c)}
 
-        anagramFinder = anagram_finder.AnagramFinder()
         result = anagramFinder._filter_anagram_lists(d)
         assert exp == result
