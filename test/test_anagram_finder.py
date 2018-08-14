61be93e671d5171b12b594b7cd442904c2bb5ba7
Craig Astill <craigastill@gmail.com>
2018-08-14 11:09:24 +0100

    Use pytest's fixtures to test all of the anagram examples
    Note: currently failing due to hardcoded anagram implementation.

--- test/test_anagram_finder.py
+++ test/test_anagram_finder.py
@@ -8,6 +8,32 @@ def anagramFinder():
     return anagram_finder.AnagramFinder()
 
 
+@pytest.fixture(params=[
+    ("the quick brown fox", {"no anagrams found", }),
+    ("eat my tea", {"eat", "eat"}),
+    ("do or door no no", {"no anagrams found", }),
+    ("pots stop pots spot stop", {"pots", "stop", "spot"}),
+    ("on pots no stop eat\nate pots spot stop tea",
+     {("on", "no"), ("pots", "stop", "spot"), ("eat", "ate" "tea")})
+])
+def anagram_examples(request):
+    return request.param
+
+
+@pytest.fixture(params=[
+    ("the quick brown fox", "no anagrams found"),
+    ("eat my tea", "eat eat"),
+    ("do or door no no", "no anagrams found"),
+    ("pots stop pots spot stop", "pots stop spot"),
+    ("on pots no stop eat\nate pots spot stop tea",
+     "on no\npots stop spot\neat ate tea")
+])
+def anagram_examples_stdout(request):
+    # TODO: move to the integration test when I need to check my stdout
+    # printer.
+    return request.param
+
+
 @pytest.mark.usefixtures("anagramFinder")
 class TestAnagramFinder(object):
     """Unittests for the AnagramFinder class."""
@@ -31,4 +57,24 @@ class TestAnagramFinder(object):
         exp = {tuple(a), tuple(c)}
 
         result = anagramFinder._filter_anagram_lists(d)
+        assert exp == result
+
+    def test_get_anagrams(self, anagramFinder):
+        """Positively verify `_get_anagrams()`."""
+        a = "stop"
+        exp = ["stop", "pots", "tops"]
+
+        result = anagramFinder._get_anagrams(a)
+        # instead of doing something like:
+        # `assert bool(x in exp for x in result)` or asserts within the for
+        # loop, I'm sorting the lists to maintain debug output.
+        exp.sort()
+        result.sort()
+        assert exp == result
+
+    def test_get_anagram_lists(self, anagramFinder, anagram_examples):
+        """Positively verify `_get_anagram_lists()`."""
+        content, exp = anagram_examples
+
+        result = anagramFinder.get_anagram_lists(content)
         assert exp == result
