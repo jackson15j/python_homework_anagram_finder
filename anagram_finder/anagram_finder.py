e9997e276f1a4afaedfeb64c7d05c0e16044bf20
Craig Astill <craigastill@gmail.com>
2018-08-14 15:43:05 +0100

    Move files into an anagram_finder/ folder.

--- anagram_finder/anagram_finder.py
+++ anagram_finder/anagram_finder.py
@@ -45,7 +45,7 @@ class AnagramFinder(object):
     # Note: Had previously used: https://github.com/dwyl/english-words/, but
     # removed it due to too many words that would not be considered standard.
     # Note: This Webster dictionary's keys/values are upper case.
-    default_en_dict = ("dictionary/graph.json")
+    default_en_dict = ("../dictionary/graph.json")
 
     def __init__(self):
         with open(self.default_en_dict, 'r') as f:
