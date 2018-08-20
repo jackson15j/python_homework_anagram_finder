5a4799a624b38d1ce06ba366510d0d088abcb218
Craig Astill <craig.astill@gmail.com>
2018-08-20 16:15:01 +0100

    [BROKEN] move Webster dictionary code out but imports break tests
    * Moved the Webster dictionary code for `get_anagrams()` to a new class.
    * Import Webster dictionary from within `AnagramFinder()`.
    * Fix path issues from previous relative path reading of the `graph.json` file.
    * Keep `AnagramFinder._get_anagrams()` to keep unittests passing.
    * FIXME: Unittests are failing due to _"off by 1"_ import differences between
      the tests and application.

--- anagram_finder/anagram_finder.py
+++ anagram_finder/anagram_finder.py
@@ -1,6 +1,7 @@
+from dictionaries.en_us_webster import EnUsWebster
+
 from orderedset import OrderedSet
 from string import punctuation
-import json
 import logging
 
 log = logging.getLogger(__name__)
@@ -22,11 +23,10 @@ class AnagramFinder(object):
     # Note: Had previously used: https://github.com/dwyl/english-words/, but
     # removed it due to too many words that would not be considered standard.
     # Note: This Webster dictionary's keys/values are upper case.
-    default_en_dict = ("dictionaries/en_us_webster/graph.json")
 
     def __init__(self):
-        with open(self.default_en_dict, 'r') as f:
-            self.en_dict_json = json.load(f)
+        # FIXME: Allow Client to define the dictionary.
+        self.anagram_lang_dict = EnUsWebster()
 
     def _filter_anagram_lists(self, anagram_list):
         """Sorts and filters the nested anagram list to remove duplicates.
@@ -57,96 +57,9 @@ class AnagramFinder(object):
         return list(ret_val)
 
     def _get_anagrams(self, word):
-        """Return a list of anagrams for the provided word.
-
-        @param str word: Word to find anagrams for.
-        @returns: list of anagrams strings for the provided `word` or `None`.
-        """
-        # Note: Testing a different english dictionary uses uppercase keys,
-        # hence the casing changes for lookups and return words.
-        upper_word = word.upper()
-        # FIXME: settle on a dictionary.
-        anagrams = [
-            x.lower() for x in self.en_dict_json.keys() if len(word) == len(x)
-            and all(char in x for char in list(upper_word))
-            and all(x.count(char) == upper_word.count(char) for char in list(upper_word))
-        ]
-
-        pluralised_anagrams = self._get_webster_pluralised_anagrams(word)
-        if pluralised_anagrams:
-            anagrams.extend(pluralised_anagrams)
-
-        webster_hacks = self._get_webster_missing_words_hack(word)
-        if webster_hacks:
-            anagrams.extend(webster_hacks)
-
-        log.debug(anagrams)
-
-        if len(anagrams) == 1:
-            return None
-        if len(anagrams) == 0:
-            # FIXME: pick an appropriate exception.
-            raise ValueError(
-                "%r, is not in the dictionary: %r" % (
-                    word, self.default_en_dict))
-        return anagrams
-
-    def _get_webster_pluralised_anagrams(self, word):
-        """The Webster dictionary from: https://github.com/adambom/dictionary,
-        does not have plurals as keys. We can solve this in two ways after
-        doing the following initial setup:
-
-        * If `word` has an `s`. Remove it and find anagrams for the reduced
-          word.
-
-        Solutions:
-
-        * Naively add an `s` to the end of all reduced anagrams.
-        * Lookup each reduced anagram and check Websters graph values for a
-          pluralisation.
-
-        Right or wrong. Going with the latter solution.
-
-        @param str word: Word to check for an `s` in and then find all plural
-                anagrams.
-        @returns: list of plural anagrams or `None`.
-        """
-        # FIXME: Webster doesn't have `TOPS` as a plural of `TOP` in it's
-        # graph.json.
-        # Note: Webster dictionary/graph keys/values are all uppercase.
-        if 's' in word:
-            temp_word = list(word)
-            temp_word.remove('s')
-            temp_word = "".join(temp_word)
-
-            anagrams = [
-                x.lower() for x in self.en_dict_json.keys()
-                if len(x) == len(temp_word)
-                and all(char in x for char in list(temp_word.upper()))]
-
-            pluralised_anagrams = []
-            for x in anagrams:
-                if "{}S".format(x.upper()) in self.en_dict_json[x.upper()]:
-                    pluralised_anagrams.append("{}s".format(x))
-            if pluralised_anagrams:
-                return pluralised_anagrams
-        return
-
-    def _get_webster_missing_words_hack(self, word):
-        """Hack to work around the the Webster dictionary from:
-        https://github.com/adambom/dictionary, not having the following words:
-
-        * "EAT"
-        * "DOOR"
-
-        @param str word: Word to check if it should return a missing word as an
-                anagram.
-        @returns: A single item list with the missing word or `None`.
-        """
-        if word in ['ate', 'eat', 'tea']:
-            return ['eat']
-        if word in ['door', 'odor']:
-            return ['door']
+        # FIXME: half-step refactor, whilst I'm pushing functionality out to a
+        # new class. Need to fix unittests before I can remove this function.
+        return self.anagram_lang_dict.get_anagrams(word)
 
     def get_anagram_lists(self, contents):
         """Return a list of sorted anagrams without any duplicates.
---
+++ anagram_finder/dictionaries/en_us_webster.py
@@ -0,0 +1,111 @@
+from os import path
+import json
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class EnUsWebster(object):
+    """
+    Anagram finding class centred on the US English Webster dictionary from:
+    https://github.com/adambom/dictionary, as a git submodule for offline
+    lookups.
+    """
+    dict_path = ("en_us_webster/graph.json")
+    dict_full_path = path.join(path.dirname(__file__), dict_path)
+
+    def __init__(self):
+        with open(self.dict_full_path, 'r') as f:
+            self.en_dict_json = json.load(f)
+
+    def get_anagrams(self, word):
+        """Return a list of anagrams for the provided word.
+
+        @param str word: Word to find anagrams for.
+        @returns: list of anagrams strings for the provided `word` or `None`.
+        """
+        # Note: Testing a different english dictionary uses uppercase keys,
+        # hence the casing changes for lookups and return words.
+        upper_word = word.upper()
+        # FIXME: settle on a dictionary.
+        anagrams = [
+            x.lower() for x in self.en_dict_json.keys() if len(word) == len(x)
+            and all(char in x for char in list(upper_word))
+            and all(x.count(char) == upper_word.count(char) for char in list(upper_word))
+        ]
+
+        pluralised_anagrams = self._get_webster_pluralised_anagrams(word)
+        if pluralised_anagrams:
+            anagrams.extend(pluralised_anagrams)
+
+        webster_hacks = self._get_webster_missing_words_hack(word)
+        if webster_hacks:
+            anagrams.extend(webster_hacks)
+
+        log.debug(anagrams)
+
+        if len(anagrams) == 1:
+            return None
+        if len(anagrams) == 0:
+            # FIXME: pick an appropriate exception.
+            raise ValueError(
+                "%r, is not in the dictionary: %r" % (
+                    word, self.default_en_dict))
+        return anagrams
+
+    def _get_webster_pluralised_anagrams(self, word):
+        """The Webster dictionary from: https://github.com/adambom/dictionary,
+        does not have plurals as keys. We can solve this in two ways after
+        doing the following initial setup:
+
+        * If `word` has an `s`. Remove it and find anagrams for the reduced
+          word.
+
+        Solutions:
+
+        * Naively add an `s` to the end of all reduced anagrams.
+        * Lookup each reduced anagram and check Websters graph values for a
+          pluralisation.
+
+        Right or wrong. Going with the latter solution.
+
+        @param str word: Word to check for an `s` in and then find all plural
+                anagrams.
+        @returns: list of plural anagrams or `None`.
+        """
+        # FIXME: Webster doesn't have `TOPS` as a plural of `TOP` in it's
+        # graph.json.
+        # Note: Webster dictionary/graph keys/values are all uppercase.
+        if 's' in word:
+            temp_word = list(word)
+            temp_word.remove('s')
+            temp_word = "".join(temp_word)
+
+            anagrams = [
+                x.lower() for x in self.en_dict_json.keys()
+                if len(x) == len(temp_word)
+                and all(char in x for char in list(temp_word.upper()))]
+
+            pluralised_anagrams = []
+            for x in anagrams:
+                if "{}S".format(x.upper()) in self.en_dict_json[x.upper()]:
+                    pluralised_anagrams.append("{}s".format(x))
+            if pluralised_anagrams:
+                return pluralised_anagrams
+        return
+
+    def _get_webster_missing_words_hack(self, word):
+        """Hack to work around the the Webster dictionary from:
+        https://github.com/adambom/dictionary, not having the following words:
+
+        * "EAT"
+        * "DOOR"
+
+        @param str word: Word to check if it should return a missing word as an
+                anagram.
+        @returns: A single item list with the missing word or `None`.
+        """
+        if word in ['ate', 'eat', 'tea']:
+            return ['eat']
+        if word in ['door', 'odor']:
+            return ['door']
