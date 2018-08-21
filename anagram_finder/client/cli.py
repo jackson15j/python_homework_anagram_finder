7d06333b9cea6b37a98758efb6532ae5085297b3
Craig Astill <craig.astill@gmail.com>
2018-08-21 20:37:47 +0100

    Plumb in & verify that clients can specify the anagram dictionary
    * CLI now has an optional flag to specify the anagram dictionary source.
    * Update CLI integration tests to verify both the default and all of the
      current anagram dictionaries.

--- anagram_finder/client/cli.py
+++ anagram_finder/client/cli.py
@@ -1,4 +1,5 @@
 from anagram_finder.anagram_core import AnagramFinder
+from anagram_finder.dictionaries.ianagram_lang_dict import AnagramLangDictEnum
 
 import click
 import logging
@@ -8,13 +9,29 @@ log = logging.getLogger(__name__)
 
 @click.command()
 @click.argument('filename', type=click.Path(exists=True))
-def input(filename):
+@click.option(
+    '--dictionary',
+    type=click.Choice([
+        'en-us-webster',
+        'source-file'
+    ]))
+def input(filename, dictionary=None):
     # TODO: check file is not empty. Failure error code.
     with open(filename, 'r') as infile:
         f = infile.read()
 
-    log.debug("Filename: %r, Content: %r", filename, f)
-    anagramFinder = AnagramFinder()
+    log.debug(
+        "Dictionary: %r, Filename: %r, Content: %r", dictionary, filename, f)
+
+    anagram_dict_enum = None
+    if dictionary == 'en-us-webster':
+        anagram_dict_enum = AnagramLangDictEnum.EN_US_WEBSTER
+    if dictionary == 'source-file':
+        anagram_dict_enum = AnagramLangDictEnum.SOURCE_FILE
+
+    anagramFinder = (
+        AnagramFinder() if anagram_dict_enum is None
+        else AnagramFinder(anagram_dict_enum))
     anagram_lists = anagramFinder.get_anagram_lists(f)
     log.debug(anagram_lists)
     printer(anagram_lists)
--- tests/test_main.py
+++ tests/test_main.py
@@ -17,7 +17,18 @@ log = logging.getLogger(__name__)
     ("../tests/data/example4.txt", b"post pots spot stop\n"),
     ("../tests/data/example5.txt", b"no on\npost pots spot stop\nate eat tea\n")
 ])
-def anagram_examples_stdout(request):
+def anagram_examples_stdout_en_us_webster_dictionary(request):
+    return request.param
+
+
+@pytest.fixture(params=[
+    ("../tests/data/example1.txt", b"no anagrams found\n"),
+    ("../tests/data/example2.txt", b"eat tea\n"),
+    ("../tests/data/example3.txt", b"no anagrams found\n"),
+    ("../tests/data/example4.txt", b"pots spot stop\n"),
+    ("../tests/data/example5.txt", b"no on\npots spot stop\nate eat tea\n")
+])
+def anagram_examples_stdout_source_file_dictionary(request):
     return request.param
 
 
@@ -38,12 +49,48 @@ class TestMain(object):
         assert b'Error: Missing argument "filename"' in cmd.stderr.read()
         assert 0 != cmd.wait()
 
-    def test_main_example(self, anagram_examples_stdout):
-        """Verifies expected stdout output of anagramsfrom each example file.
+    def test_main_example_default_anagram_dict(
+            self, anagram_examples_stdout_en_us_webster_dictionary):
+        """Verifies expected stdout output of anagrams from each example file
+        against the default anagram dictionary.
         """
-        filepath, exp = anagram_examples_stdout
+        filepath, exp = anagram_examples_stdout_en_us_webster_dictionary
         cmd = subprocess.Popen(
             ["pipenv", "run", "python", "main.py", filepath],
+            cwd=self.base_module_path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+        assert exp == cmd.stdout.read()
+        assert b'' == cmd.stderr.read()
+        assert 0 == cmd.wait()
+
+    def test_main_example_en_us_webster_anagram_dict(
+            self, anagram_examples_stdout_en_us_webster_dictionary):
+        """Verifies expected stdout output of anagrams from each example file
+        against the EN US Webster anagram dictionary.
+        """
+        filepath, exp = anagram_examples_stdout_en_us_webster_dictionary
+        cmd = subprocess.Popen(
+            ["pipenv", "run", "python", "main.py",
+             "--dictionary=en-us-webster", filepath],
+            cwd=self.base_module_path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+        assert exp == cmd.stdout.read()
+        assert b'' == cmd.stderr.read()
+        assert 0 == cmd.wait()
+
+    def test_main_example_source_file_anagram_dict(
+            self, anagram_examples_stdout_source_file_dictionary):
+        """Verifies expected stdout output of anagrams from each example file
+        against the source file as the anagram dictionary.
+        """
+        filepath, exp = anagram_examples_stdout_source_file_dictionary
+        cmd = subprocess.Popen(
+            ["pipenv", "run", "python", "main.py", "--dictionary=source-file",
+             filepath],
             cwd=self.base_module_path,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE)
