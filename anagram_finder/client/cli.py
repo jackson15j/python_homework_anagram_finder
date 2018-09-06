add0cdca833ab97b3a5829cdd2eba96f1817c32b
Craig Astill <craig.astill@gmail.com>
2018-09-06 09:08:35 +0100

    Separate click UI from CLI logic, in preparation for testing
    Wanted to test the CLI in isolation (instead of the current
    `test_main.py`. Needed to separate out the `click` UI logic and make the
    AnagramFinder path configurable in preparation for a mocked REST server test.
    
    NOTE: this has increased the number of validations that need to be incorporated
    into the non-`click` UI logic.

--- anagram_finder/client/cli.py
+++ anagram_finder/client/cli.py
@@ -8,7 +8,71 @@ import logging
 import requests
 
 log = logging.getLogger(__name__)
-base_path = "http://127.0.0.1:5000"
+
+
+class Cli(object):
+    """CLI Client class that makes a GET REST API request to the AnagramFinder
+    service and then renders the results to the console (One anagram list per
+    line).
+    """
+    def __init__(self, base_path="http://127.0.0.1:5000"):
+        self.base_path = base_path
+
+    def input(self, filename, dictionary=None):
+        """Wrapper function that:
+
+        * Reads in the input file.
+        * Selects REST path from selected input dictionary.
+        * Does AnagramFinder GET request.
+        * Calls `printer()` to display to stdout.
+
+        # TODO: separate out LHS file read and RHS network activity.
+        # TODO: use enums for dictionary, now that the input checking has been
+        # separated out.
+
+        @param str filename: File path to a file containing words that anagrams
+                need to be found for.
+        @param str dictionary: Name of dictionary to use.
+        """
+        # TODO: check file is not empty. Failure error code.
+        with open(filename, 'r') as infile:
+            f = infile.read()
+
+        log.debug(
+            "Dictionary: %r, Filename: %r, Content: %r", dictionary, filename,
+            f)
+
+        path = "%s/anagrams/%s" % (self.base_path, dictionary)
+        if dictionary is None:
+            path = "%s/anagrams/%s" % (self.base_path, 'en-us-webster')
+
+        print(path)
+        # urllib library
+        # response = urllib.request.urlopen("%s/%s" % (path, f))
+        # content = response.read().decode('utf-8')
+
+        # FIXME: Use query form in request: "url?query_string", instead of:
+        # "url/query_string".
+
+        # requests library.
+        response = requests.get("%s/%s" % (path, f))
+        content = response.content.decode('utf-8')
+        # TODO: add response code check.
+        log.debug(content)
+
+        anagram_lists = json.loads(content)
+        log.debug(anagram_lists)
+        self.printer(anagram_lists)
+
+    def printer(self, anagram_lists):
+        """Munges the input into a list of anagrams per line. Which is then
+        printed to stdout.
+
+        @param list anagram_lists: Expects output from:
+                `AnagramFinder().get_anagram_lists()`.
+        """
+        for anagram_list in anagram_lists:
+            click.echo(" ".join(anagram_list))
 
 
 @click.command()
@@ -20,45 +84,7 @@ base_path = "http://127.0.0.1:5000"
         'source-file'
     ]))
 def input(filename, dictionary=None):
-    # TODO: check file is not empty. Failure error code.
-    with open(filename, 'r') as infile:
-        f = infile.read()
-
-    log.debug(
-        "Dictionary: %r, Filename: %r, Content: %r", dictionary, filename, f)
-
-    path = "%s/anagrams/%s" % (base_path, dictionary)
-    if dictionary is None:
-        path = "%s/anagrams/%s" % (base_path, 'en-us-webster')
-
-    print(path)
-    # urllib library
-    # response = urllib.request.urlopen("%s/%s" % (path, f))
-    # content = response.read().decode('utf-8')
-
-    # FIXME: Use query form in request: "url?query_string", instead of:
-    # "url/query_string".
-
-    # requests library.
-    response = requests.get("%s/%s" % (path, f))
-    content = response.content.decode('utf-8')
-    # TODO: add response code check.
-    log.debug(content)
-
-    anagram_lists = json.loads(content)
-    log.debug(anagram_lists)
-    printer(anagram_lists)
-
-
-def printer(anagram_lists):
-    """Munges the input into a list of anagrams per line. Which is then printed
-    to stdout.
-
-    @param list anagram_lists: Expects output from:
-            `AnagramFinder().get_anagram_lists()`.
-    """
-    for anagram_list in anagram_lists:
-        click.echo(" ".join(anagram_list))
+    return Cli().input(filename, dictionary)
 
 
 def main():
